80f1b9a15093b491bc4e78701be4ff7b5c680467
Feed-dev <federico.lubbert@gmail.com>
2023-01-04 20:51:34 +0100

    little changes in Recieve send contract

--- contracts/Recieve-Send-eth.sol
+++ contracts/Recieve-Send-eth.sol
@@ -42,10 +42,10 @@ contract SendEther {
         require(sent, "Failed to send Ether");
     }
 
-    function sendViaCall(address payable _to) public payable {
-        // Call returns a boolean value indicating success or failure.
-        // This is the current recommended method to use.
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
-    }
+    // function sendViaCall(address payable _to) public payable {
+    //     // Call returns a boolean value indicating success or failure.
+    //     // This is the current recommended method to use.
+    //     (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+    //     require(sent, "Failed to send Ether");
+    // }
 }
