44f53b4d9c6bc8757d8c52c463d7ba465d1bffdb
Feed-dev <federico.lubbert@gmail.com>
2023-01-12 09:14:20 +0100

    removed receiver contract from Call.sol

--- contracts/Call.sol
+++ contracts/Call.sol
@@ -1,23 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Receiver {
-    event Received(address caller, uint amount, string message);
-
-    fallback() external payable {
-        emit Received(msg.sender, msg.value, "Fallback was called");
-    }
-
-    function foo(
-        string memory _message,
-        uint _x
-    ) public payable returns (uint) {
-        emit Received(msg.sender, msg.value, _message);
-
-        return _x + 1;
-    }
-}
-
 contract Caller {
     event Response(bool success, bytes data);
 
