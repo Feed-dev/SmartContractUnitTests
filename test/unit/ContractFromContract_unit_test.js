6f9e73e0ba337f9b0737e99aa54fb552960686d2
Feed-dev <federico.lubbert@gmail.com>
2023-01-21 12:19:11 +0100

    changed unit test for car and carfactory contract

--- test/unit/ContractFromContract_unit_test.js
+++ test/unit/ContractFromContract_unit_test.js
@@ -3,43 +3,58 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers } = require("hardhat");
 
 describe("Car and CarFactory Contract", function () {
-  let car, carFactory, owner, model, carAddr, salt;
+  let carFactory;
+  let owner;
+  let carAddress;
+  let recipient;
+  let salt;
 
   async function deployCarFixture() {
-    const carFactory = await ethers.getContractFactory("CarFactory");
-    carFactory = await carFactory.deploy();
+    const carFactoryFactory = await ethers.getContractFactory("CarFactory");
+    carFactory = await carFactoryFactory.deploy();
     await carFactory.deployed();
-    owner = ethers.Wallet.createRandom().address;
-    model = "Tesla Model S";
-    salt = ethers.utils.formatBytes32String("uniqueSalt");
-    await carFactory.create2AndSendEther(owner, model, salt, {
-      value: ethers.utils.parseEther("1"),
-    });
-    carAddr = await carFactory.getCar(0);
-    car = await new ethers.Contract(carAddr, Car.abi, ethers.provider);
-    return { car, carFactory };
+    owner = await ethers.getSigner();
+    recipient = ethers.Wallet.createRandom().address;
+    salt = ethers.utils.solidityKeccak256(["bytes32"], ["random"]);
+    return { carFactory };
   }
 
+  it("should create a new car", async function () {
+    const { carFactory } = await loadFixture(deployCarFixture);
+    await carFactory.create(owner.address, "Tesla Model S");
+    const car = await carFactory.cars(0);
+    expect(car.owner).to.equal(owner.address);
+    expect(car.model).to.equal("Tesla Model S");
+  });
+
   it("should create a new car and deposit ether to the car contract", async function () {
-    const { car, carFactory } = await loadFixture(deployCarFixture);
-    const carInfo = await carFactory.getCar(0);
-    expect(carInfo.owner).to.equal(owner);
-    expect(carInfo.model).to.equal(model);
-    expect(carInfo.carAddr).to.equal(carAddr);
-    expect(carInfo.balance).to.equal(ethers.utils.parseEther("1"));
+    const { carFactory } = await loadFixture(deployCarFixture);
+    await carFactory.createAndSendEther(owner.address, "Tesla Model S", {
+      value: ethers.utils.parseEther("1"),
+    });
+    const car = await carFactory.cars(0);
+    carAddress = car.carAddr;
+    const balance = await ethers.provider.getBalance(carAddress);
+    expect(balance).to.be.above(0);
   });
 
   it("should not allow to call non-payable function with ether", async function () {
-    const { car } = await loadFixture(deployCarFixture);
-    await expect(car.nonPayable({ value: ethers.utils.parseEther("1") })).to.be
-      .rejected;
+    const { carFactory } = await loadFixture(deployCarFixture);
+    await expect(
+      carFactory.create(owner.address, "Tesla Model S", {
+        value: ethers.utils.parseEther("1"),
+      })
+    ).to.be.rejected;
   });
 
   it("should transfer ether from the car contract to another address", async function () {
-    const { car } = await loadFixture(deployCarFixture);
-    const recipient = ethers.Wallet.createRandom().address;
+    const { carFactory } = await loadFixture(deployCarFixture);
     const initialBalance = await ethers.provider.getBalance(recipient);
-    await car.transfer(recipient, ethers.utils.parseEther("1"));
+    await carFactory.transfer(
+      carAddress,
+      recipient,
+      ethers.utils.parseEther("1")
+    );
     const finalBalance = await ethers.provider.getBalance(recipient);
     expect(finalBalance).to.be.above(initialBalance);
   });
