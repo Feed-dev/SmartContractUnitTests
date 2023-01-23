e889cafe3c794dea3e9810beb78eac9fbac28ed4
Feed-dev <federico.lubbert@gmail.com>
2023-01-23 10:17:05 +0100

    edited unit tests for Library.sol

--- test/unit/Library_unit_test.js
+++ test/unit/Library_unit_test.js
@@ -22,28 +22,3 @@ describe("Math Library Unit Tests", function () {
     expect(result.toString()).to.equal("4");
   });
 });
-
-describe("Array Library Unit Tests", function () {
-  async function deployArrayLibraryFixture() {
-    const testArrayFactory = await ethers.getContractFactory("TestArray");
-    const testArray = await testArrayFactory.deploy();
-    await testArray.deployed();
-    return { testArray };
-  }
-
-  it("should remove the element at index 1 and re-organize the array correctly", async function () {
-    const { testArray } = await loadFixture(deployArrayLibraryFixture);
-    await testArray.testArrayRemove();
-    const arr = await testArray.arr();
-    expect(arr.length).to.equal(2);
-    expect(arr[0]).to.equal("0");
-    expect(arr[1]).to.equal("2");
-  });
-
-  it("should throw an error when trying to remove an element from an empty array", async function () {
-    const { testArray } = await loadFixture(deployArrayLibraryFixture);
-    await expect(testArray.testArrayRemove()).to.be.rejectedWith(
-      "Can't remove from empty array"
-    );
-  });
-});
