6bdda2b5c19d16dd540552b0a2ff53ce72ebbc11
Feed-dev <federico.lubbert@gmail.com>
2023-01-24 11:14:04 +0100

    added more unit tests

--- test/unit/EtherWallet_unit_test.js
+++ test/unit/EtherWallet_unit_test.js
@@ -1,3 +1,83 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+
+describe("EtherWallet Contract Unit Tests", function () {
+  async function deployEtherWalletFixture() {
+    const etherWalletFactory = await ethers.getContractFactory("EtherWallet");
+    const etherWallet = await etherWalletFactory.deploy();
+    await etherWallet.deployed();
+    return { etherWallet };
+  }
+  it("should set the msg.sender as the owner of the contract", async function () {
+    const { etherWallet } = await loadFixture(deployEtherWalletFixture);
+    const expectedOwner = ethers.wallet.address;
+    expect(await etherWallet.owner()).to.equal(expectedOwner);
+  });
+
+  it("should allow the owner to deposit funds", async function () {
+    const { etherWallet } = await loadFixture(deployEtherWalletFixture);
+    const initialBalance = await ethers.getBalance(etherWallet.address);
+    const depositAmount = ethers.utils.parseEther("1.0");
+    await ethers.provider.send("eth_sendTransaction", [
+      {
+        from: ethers.wallet.address,
+        to: etherWallet.address,
+        value: depositAmount,
+        gasPrice: ethers.utils.parseUnits("20", "gwei"),
+        gas: 21000,
+      },
+    ]);
+    const finalBalance = await ethers.getBalance(etherWallet.address);
+    expect(finalBalance.sub(initialBalance)).to.equal(depositAmount);
+  });
+
+  it("should allow the owner to withdraw funds", async function () {
+    const { etherWallet } = await loadFixture(deployEtherWalletFixture);
+    await ethers.provider.send("eth_sendTransaction", [
+      {
+        from: ethers.wallet.address,
+        to: etherWallet.address,
+        value: ethers.utils.parseEther("1.0"),
+        gasPrice: ethers.utils.parseUnits("20", "gwei"),
+        gas: 21000,
+      },
+    ]);
+    const initialBalance = await ethers.getBalance(ethers.wallet.address);
+    const amountToWithdraw = ethers.utils.parseEther("0.5");
+    await etherWallet.withdraw(amountToWithdraw);
+    const finalBalance = await ethers.getBalance(ethers.wallet.address);
+    expect(finalBalance.sub(initialBalance)).to.equal(amountToWithdraw);
+  });
+
+  it("should not allow non-owners to withdraw funds", async function () {
+    const { etherWallet } = await loadFixture(deployEtherWalletFixture);
+    await ethers.provider.send("eth_sendTransaction", [
+      {
+        from: ethers.wallet.address,
+        to: etherWallet.address,
+        value: ethers.utils.parseEther("1.0"),
+        gasPrice: ethers.utils.parseUnits("20", "gwei"),
+        gas: 21000,
+      },
+    ]);
+    const randomWallet = ethers.Wallet.createRandom();
+    await expect(
+      etherWallet.connect(randomWallet).withdraw(ethers.utils.parseEther("0.5"))
+    ).to.be.rejectedWith("caller is not owner");
+  });
+  it("should return the correct balance", async function () {
+    const { etherWallet } = await loadFixture(deployEtherWalletFixture);
+    await ethers.provider.send("eth_sendTransaction", [
+      {
+        from: ethers.wallet.address,
+        to: etherWallet.address,
+        value: ethers.utils.parseEther("1.0"),
+        gasPrice: ethers.utils.parseUnits("20", "gwei"),
+        gas: 21000,
+      },
+    ]);
+    const expectedBalance = await ethers.getBalance(etherWallet.address);
+    expect(await etherWallet.getBalance()).to.equal(expectedBalance);
+  });
+});
